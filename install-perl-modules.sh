4fb652abcef05fa038dd27aaf5e2c3ee7cccc991
Rob Hoelz <rob@hoelz.ro>
2011-10-10 20:23:00 -0500

    Add missing dependencies for my Vim config

--- install-perl-modules.sh
+++ install-perl-modules.sh
@@ -6,4 +6,4 @@ if [[ ! -z "$PATH_WITHOUT_PERLBREW" ]]; then
     PERL=$(PATH=$PATH_WITHOUT_PERLBREW which perl)
 fi
 
-exec $PERL $(which cpanm) -l ~/.vim/perl-lib DBI DBD::SQLite Class::Accessor::Fast namespace::clean local::lib
+exec $PERL $(which cpanm) -l ~/.vim/perl-lib DBI DBD::SQLite Class::Accessor::Fast namespace::clean local::lib List::MoreUtils Params::Util Sub::Install
