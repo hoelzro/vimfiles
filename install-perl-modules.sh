3fbe3b9edc8801bda97b9eb483a08b72e8a6f2a5
Rob Hoelz <rob@hoelz.ro>
2011-10-10 20:20:23 -0500

    Better "system" Perl detection

--- install-perl-modules.sh
+++ install-perl-modules.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-exec /usr/bin/perl $(which cpanm) -l ~/.vim/perl-lib DBI DBD::SQLite Class::Accessor::Fast namespace::clean local::lib
+PERL=/usr/bin/perl
+
+if [[ ! -z "$PATH_WITHOUT_PERLBREW" ]]; then
+    PERL=$(PATH=$PATH_WITHOUT_PERLBREW which perl)
+fi
+
+exec $PERL $(which cpanm) -l ~/.vim/perl-lib DBI DBD::SQLite Class::Accessor::Fast namespace::clean local::lib
