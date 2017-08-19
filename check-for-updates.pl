472946051fe4ffe3507c90a01f3181cf03d3ac83
Rob Hoelz <rob@hoelz.ro>
2017-08-18 21:44:45 -0500

    Modify my check-for-updates script
    ...to actually check my real plugins for updates.

--- check-for-updates.pl
+++ check-for-updates.pl
@@ -1,77 +1,122 @@
 #!/usr/bin/env perl
 
-use 5.12.0;
-use autodie qw(open opendir);
+use autodie;
+use strict;
 use warnings;
+use feature qw(say);
+use experimental 'signatures';
 
-use AnyEvent;
-use AnyEvent::HTTP qw(http_get);
-use Hash::AsObject;
+use FindBin;
+use File::Spec;
+use List::Util qw(min);
+use Mojo::UserAgent;
 
-my @plugins;
-my %plugin_set;
-my $condvar = AnyEvent->condvar;
-my $plugins_to_check;
-my %watchers;
+use Data::Printer;
 
-my $fh;
-open $fh, '<', 'plugins';
-while(<$fh>) {
+my $ua = Mojo::UserAgent->new;
+
+sub github_version_checker($url) {
+    my ( $user, $repo ) = $url =~ m{github[.]com/([^/]+)/([^/]+)};
+    my $repo_data = $ua->get("https://api.github.com/repos/$user/$repo")->res->json;
+    my $default_branch = $repo_data->{'default_branch'};
+    my $branch_data = $ua->get("https://api.github.com/repos/$user/$repo/git/refs/heads/$default_branch")->res->json;
+    return $branch_data->{'object'}{'sha'};
+}
+
+sub drchip_version_checker($full_url) {
+    my ( $url, $fragment ) = $full_url =~ /^(.*)#(.*)$/;
+
+    my $dom = $ua->get($url)->res->dom;
+
+    my $version_table = $dom->find("a[name=$fragment]")->first->next;
+    my $version_info = $version_table->all_text;
+    unless($version_info =~ /Updated.*\(v([^)]+)\)/) {
+        die "'$version_info' doesn't appear to contain versioning information!";
+    }
+
+    return $1;
+}
+
+sub vim_org_scripts_version_checker($url) {
+    my $dom = $ua->get($url)->res->dom;
+
+    my $package_header = $dom->find('table > tr.tableheader th')->grep(sub($th) {
+        $th->text eq 'package'
+    })->first;
+    my $release_history_table = $package_header->ancestors('table')->first;
+    return $release_history_table->find('tr:not(.tableheader) td:nth-child(2)')->first->all_text;
+}
+
+sub get_version_checker($url) {
+    if($url =~ /github[.]com/) {
+        return \&github_version_checker;
+    } elsif($url =~ /drchip[.]org/) {
+        return \&drchip_version_checker;
+    } elsif($url =~ m{www[.]vim[.]org/scripts}) {
+        return \&vim_org_scripts_version_checker;
+    }
+    die "I don't know how to handle $url";
+}
+
+sub git_sha_comparator($lhs, $rhs) {
+    my $prefix_length = min(length($lhs), length($rhs));
+
+    return substr($lhs, 0, $prefix_length) eq substr($rhs, 0, $prefix_length);
+}
+
+sub full_string_comparator($lhs, $rhs) {
+    return $lhs eq $rhs;
+}
+
+sub get_version_comparator($url) {
+    if($url =~ /github[.]com/) {
+        return \&git_sha_comparator;
+    } elsif($url =~ /drchip[.]org/) {
+        return \&full_string_comparator;
+    } elsif($url =~ m{www[.]vim[.]org/scripts}) {
+        return \&full_string_comparator;
+    }
+    die "I don't know how to handle $url";
+}
+
+my @plugins_to_check;
+
+open my $pipe, '-|', 'git', 'ls-files', File::Spec->catdir($FindBin::Bin, 'bundle');
+my $current_plugin = '';
+while(<$pipe>) {
     chomp;
-    my ( $plugin, $version, $type, $source ) = split /:/, $_, 4;
 
-    push @plugins, Hash::AsObject->new({
-        name => $plugin,
-        version => $version,
-        type => $type,
-        source => $source,
-    });
-    $plugin_set{$plugin} = 1;
-}
-close $fh;
-
-my $dir;
-opendir $dir, 'bundle';
-while(my $name = readdir $dir) {
-    next if $name =~ /^\./;
-    unless(exists $plugin_set{$name}) {
-        say "$name not found in plugins file!";
+    if(m{^(bundle/([^/]+))}) {
+        my ( $plugin_path, $plugin_name ) = ( $1, $2 );
+        next if $current_plugin eq $plugin_name;
+        $current_plugin = $plugin_name;
+        $plugin_path = File::Spec->catdir($FindBin::Bin, $plugin_path);
+        my $upstream_path = File::Spec->catfile($plugin_path, '.upstream');
+        unless(-e $upstream_path) {
+            die "No .upstream file for $plugin_name; exiting\n";
+        }
+        open my $fh, '<', $upstream_path;
+        my $line = <$fh>;
+        close $fh;
+        next unless defined $line; # my own plugin, managed here
+        chomp $line;
+        my ( $upstream_url, $version ) = split /\s+/, $line;
+        push @plugins_to_check, [ $plugin_path, $plugin_name, $upstream_url, $version ];
+    } else {
+        die "'$_' doesn't match my plugin regex";
     }
 }
-closedir $dir;
+close $pipe;
 
-$plugins_to_check = @plugins;
+for my $plugin (@plugins_to_check) {
+    my ( $path, $name, $upstream, $version ) = @$plugin;
 
-sub end_loop_if_done {
-    unless(--$plugins_to_check) {
-        $condvar->broadcast;
+    my $version_checker = get_version_checker($upstream);
+    my $version_cmp = get_version_comparator($upstream);
+
+    my $upstream_version = $version_checker->($upstream);
+
+    if(!$version_cmp->($upstream_version, $version)) {
+        say "$name is out of date (us: $version upstream: $upstream_version)";
     }
 }
-
-sub check_me {
-    end_loop_if_done;
-}
-
-sub check_vimscript {
-    my ( $script_id ) = @_;
-    http_get "http://www.vim.org/scripts/script.php?script_id=$script_id", sub {
-        my ( $data ) = @_;
-        end_loop_if_done;
-    };
-}
-
-foreach my $plugin (@plugins) {
-    my $type = $plugin->type;
-    my $checker = __PACKAGE__->can('check_' . $type);
-    unless($checker) {
-        say "Unknown source type '$type'";
-        $plugins_to_check--;
-        next;
-    }
-    $checker->($plugin->source);
-}
-
-$condvar->wait;
-
-__DATA__
-- verbose option (prints what was found)
