d8fd3094b43fb7cb840d0ed2aa3b6e10bde391aa
Rob Hoelz <rob@hoelz.ro>
2020-11-20 17:51:27 -0600

    Go LSP: Fall back to file directory if no root dir found

--- bundle/nvim-lspconfig/lua/nvim_lsp/gopls.lua
+++ bundle/nvim-lspconfig/lua/nvim_lsp/gopls.lua
@@ -1,11 +1,27 @@
 local configs = require 'nvim_lsp/configs'
 local util = require 'nvim_lsp/util'
 
+local function find_root_dir(...)
+  local root_dir = util.root_pattern('go.mod', '.git')(...)
+  if not root_dir then
+    local filename = ...
+    local final_slash = string.find(string.reverse(filename), '/')
+    if final_slash then
+      final_slash = #filename - final_slash
+      local dirname = string.sub(filename, 1, final_slash + 1)
+      return dirname
+    else
+      return './'
+    end
+  end
+  return root_dir
+end
+
 configs.gopls = {
   default_config = {
     cmd = {"gopls"};
     filetypes = {"go", "gomod"};
-    root_dir = util.root_pattern("go.mod", ".git");
+    root_dir = find_root_dir;
   };
   -- on_new_config = function(new_config) end;
   -- on_attach = function(client, bufnr) end;
